7cc86471e3a7ddf1273b6f21b183f4608b1bdfb7
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-03-11 00:25:28 +0100

    refactor(init): remove AppSettings from exports in live module

--- src/live/__init__.py
+++ src/live/__init__.py
@@ -4,11 +4,9 @@ from .aggregator import LiveAggregator
 from .providers import FastF1Provider, OpenF1Provider, ProviderError
 from .signalr_provider import UnofficialF1SignalRProvider
 from .service import LiveService
-from .settings import AppSettings
 from .sse import SSEBroadcaster
 
 __all__ = [
-    "AppSettings",
     "FastF1Provider",
     "LiveAggregator",
     "LiveService",
